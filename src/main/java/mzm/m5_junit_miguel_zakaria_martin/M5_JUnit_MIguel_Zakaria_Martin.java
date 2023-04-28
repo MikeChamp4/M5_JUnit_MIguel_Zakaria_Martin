fb682f568a0c52664b4a681879355351c70f3c59
MikeChamp4 <zambranocmiguele@gmail.com>
2023-04-28 12:43:19 +0200

    first commit

---
+++ src/main/java/mzm/m5_junit_miguel_zakaria_martin/M5_JUnit_MIguel_Zakaria_Martin.java
@@ -0,0 +1,8 @@
+package mzm.m5_junit_miguel_zakaria_martin;
+
+public class M5_JUnit_MIguel_Zakaria_Martin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
