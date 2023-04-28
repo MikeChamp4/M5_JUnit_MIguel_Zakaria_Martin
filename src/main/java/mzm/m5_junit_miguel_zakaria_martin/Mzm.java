02cb46b5e30ad72f0c4ef532e8ddc6a092117167
MikeChamp4 <zambranocmiguele@gmail.com>
2023-04-28 13:10:15 +0200

    metodos area y perimetro del circulo

--- src/main/java/mzm/m5_junit_miguel_zakaria_martin/Mzm.java
+++ src/main/java/mzm/m5_junit_miguel_zakaria_martin/Mzm.java
@@ -2,4 +2,17 @@ package mzm.m5_junit_miguel_zakaria_martin;
 
 public class Mzm{
 
+    public double calcularAreaCirculo(double radio){
+
+        double areaCirculo = 0.0;
+        areaCirculo = Math.PI * (radio * 2);
+        return areaCirculo;
+    }
+
+    public double calcularPerimetroCiruculo(double radio){
+
+        double perimetroCiruculo = 0.0;
+        perimetroCiruculo = (2 * Math.PI) * radio;
+        return perimetroCiruculo;
+    }
 }
